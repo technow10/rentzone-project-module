e959ce1d2d18d1257788ee3d73be5f4bb02b208c
finest <finesttechnologyconsultingllc@gmail.com>
2025-01-26 08:48:24 -0600

    Update infrastructure

---
+++ VPC/variable.tf
@@ -0,0 +1,13 @@
+# Create Environment variables for this project
+variable "region" {}
+variable "project_name" {}
+variable "environment" {}
+
+# VPC variables
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_app_subnet_az1_cidr" {}
+variable "private_app_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
---
+++ security-group/variable.tf
@@ -0,0 +1,5 @@
+# Create variables for security groups/ do not add all the resources created
+variable "project_name" {}
+variable "environment" {}
+variable "vpc_id" {}
+variable "ssh-ip" {}
